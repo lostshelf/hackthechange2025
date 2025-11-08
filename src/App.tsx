1590afe6d32d66143bcfbc44e0f3cf7f0c5d3403
Mirza <mirzabaig2006@hotmail.com>
2025-11-08 11:37:45 -0700

    Test Push

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      Hello from Mirza 👋👋👋
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
