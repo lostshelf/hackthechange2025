7061cb028b912202d7a035bc6fe85fa7ef0bf4a7
Mirza <mirzabaig2006@hotmail.com>
2025-11-08 16:58:31 -0700

    Form Creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const IssueState = {
+  UNATTENDED: "Unattended",
+  IN_PROGRESS: "In progress",
+  RESOLVED: "Resolved"
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
 
-      Hello from Mirza 👋👋👋
+    // parent div
+    <div className="font-light w-full overflow-x-hidden m-0 p-0">
+      <div className="grid grid-cols-[70%_30%]">
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* Image and Description */}
+        <div className="grid grid-cols-1">
+          <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" className="h-full w-full object-scale-down" />
+
+          <div className="bg-gray-950 text-left pl-2">
+            <p className="text-lg">INSERT TITLE HERE</p>
+            <p className="">INSERT SUMMARY HERE</p>
+          </div>
+
+        </div>
+
+        {/* Column on the right */}
+        <div className="container bg-gray-900 text-left m-0 object-cover">
+          <p className="pl-2 m-2">
+            Current State: {IssueState.UNATTENDED}
+          </p>
+
+          <div className="container text-xs bg-black w-full h-full p-3">
+            {/* A discussion post*/}
+            <div>
+              <img 
+              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              className="w-10 rounded-full"/>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
