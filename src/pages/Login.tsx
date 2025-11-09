173c9c1a5924b239efe0418d28e45875103221e7
Talhah <talhah.ahmad1@ucalgary,ca>
2025-11-08 17:26:16 -0700

    blsh blsh blsh

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,42 @@
+import "../App.tsx";
+import {useState} from "react";
+
 function LoginPage() {
     // Create the login page here
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        // TODO: Send this to the server for verification
+        console.log(`Inputted email is: ${email}`);
+        console.log(`Inputted password is: ${password}`);
+    }
+
     return (
         <>
+            <div className="mb-6">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Email address
+                </label>
+                <input type="email" id="email"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       required/>
+            </div>
+
+            <div className="mb-6">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Password
+                </label>
+                <input type="password" id="password"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       required/>
+            </div>
+
+            <button type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={handleLogin}>
+                Enter
+            </button>
         </>
     )
 }
