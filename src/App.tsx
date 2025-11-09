bbe03dd45c51f823081449c3cea371c4b213761b
Talhah <talhah.ahmad1@ucalgary,ca>
2025-11-08 21:15:19 -0700

    Added Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import HomePage from './pages/Home';
 import LandingPage from './pages/Landing';
 import LoginPage from './pages/Login';
 
+import './App.css'
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+      <>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/home" element={<HomePage />} />
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="*" element={<h1>404 Not Found</h1>} />
+              </Routes>
+          </BrowserRouter>
+
+      </>
+
   );
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,64 @@
-import "../App.tsx";
+import "../App.css";
 import {useState} from "react";
 
 function LoginPage() {
-    // Create the login page here
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin = () => {
-        // TODO: Send this to the server for verification
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // TODO: Verify information through the server
         console.log(`Inputted email is: ${email}`);
         console.log(`Inputted password is: ${password}`);
     }
 
     return (
-        <>
-            <div className="mb-6">
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Email address
+        <div className="bg-gray-200 border-b border-gray-300 rounded-lg p-2 h-screen text-center items-center justify-center font-sans">
+            <h1 className="text-2xl font-bold mb-6 mt-10 text-center text-red-600">
+                Enter Log In Information:
+            </h1>
+
+            {/* Input label for email address */}
+            <div className="mb-6 items-center flex justify-center">
+                <label htmlFor="email" className="mr-3 font-medium bold text-blue-400 w-24 text-right">
+                    Email
                 </label>
                 <input type="email" id="email"
-                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       required/>
+                       className="bg-white border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+                       required
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
 
-            <div className="mb-6">
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            {/* Input label for password */}
+            <div className="mb-6 items-center flex justify-center">
+                <label htmlFor="password" className="mr-3 font-medium bold text-blue-400 w-24 text-right">
                     Password
                 </label>
                 <input type="password" id="password"
-                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       required/>
+                       className="bg-white border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+                       required
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
             </div>
 
+            {/* Create new user */}
+            {/* TODO: Make button change page */}
             <button type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg px-5 py-2.5 mr-5 text-center"
+                    onClick={() => console.log("new")}>
+                Create Account
+            </button>
+
+            {/* Submit login information */}
+            <button type="submit"
+                    className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg px-5 py-2.5 text-center"
                     onClick={handleLogin}>
                 Enter
             </button>
-        </>
+        </div>
     )
 }
 
